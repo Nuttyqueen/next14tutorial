e9e263b2116b6efe550bec7f4db8b7710d0602d9
Nuttyqueen <pair25525@gmail.com>
2024-05-17 10:33:42 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
@@ -10,22 +9,10 @@ export const metadata = {
   title: "Next Nut PJ",
   description: "Next.js starter app",
 };
-=======
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Next App',
-  description: 'Next.js starter app',
-}
->>>>>>> 52f586144dafb62c071571e2d71dfda76e45b5c8
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-<<<<<<< HEAD
       <body className={inter.className}>
         <div className="container">
           <Navbar />
@@ -36,9 +23,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-=======
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
-}
->>>>>>> 52f586144dafb62c071571e2d71dfda76e45b5c8
